9a189f3606571207d9aad41d95b8c467a5df1066
kh429221 <kh429221@students.mimuw.edu.pl>
2024-01-19 19:40:43 +0100

    input in tests

--- test_good.sh
+++ test_good.sh
@@ -5,6 +5,10 @@ for f in $FILES
 do
     CORE=${f%.*}
     ./latc_x86_64 $f
-    $CORE >$CORE.out
+    if [ -f $CORE.input ]; then
+        $CORE <$CORE.input >$CORE.out
+    else
+        $CORE >$CORE.out
+    fi
     diff $CORE.out $CORE.output
 done
